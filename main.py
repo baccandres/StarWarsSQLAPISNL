b7533b81d0ea840ea9ffbbc7af2297dcf9d8520d
baccandres <andresuzb@gmail.com>
2021-11-29 18:38:35 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask import jsonify
 import tools.sqltools as sqt
+import markdown.extensions.fenced_code
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
     return "Ciao Mondo!"
-
 
 @app.route("/characters")
 def gimme_characters():
@@ -19,7 +19,15 @@ def gimme_quotes(character):
     x = sqt.character_quotes(character)
     return jsonify(x)
 
+@app.route("/sentiment/<character>")
+def gimme_sentiment(character):
+    x = sqt.polarity(character)
+    return jsonify(x)
 
+@app.route("/newcharacter", methods=["POST"])
+def newCharacter():
+    x = request.form.get("Campo")
+    return sqt.insertCharacter(x)
 
 
 
--- tools/sqltools.py
+++ tools/sqltools.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from config.configuration import engine
+
+analyser = SentimentIntensityAnalyzer()
 
 def characters_list():
     query = list(engine.execute("SELECT `Name` FROM `Starwars`.`Character`;"))
@@ -14,3 +18,49 @@ def character_quotes(character):
     WHERE `Name` = '{character}';"""))
     list_ = [{'Quote': i[1]} for i in query]
     return list_
+
+def polarity(character):
+    query = list(engine.execute(f"""SELECT `Name`, `Dialogue`.`Dialogue`
+    FROM `Starwars`.`Character`
+    INNER JOIN `Dialogue`
+    ON `idCharacter` = `Character_idCharacter`
+    WHERE `Name` = '{character}';"""))
+    x = [analyser.polarity_scores(i[1])['compound'] for i in query]
+    
+    if np.mean(x) >= .05:
+        return {'Positive':round(np.mean(x),3)}
+    elif np.mean(x) <= -.05:
+        return {'Negative':round(np.mean(x),3)}
+    else:
+        return {'Neutral':round(np.mean(x),3)}
+    
+
+### POST ###
+def check(column,string):
+    if column == "character":
+        query = list(engine.execute(f"SELECT `Name` FROM `Starwars`.`Character` WHERE `Name` = '{string}';"))
+        if len(query) > 0:
+            return True
+        else:
+            return False
+        
+    elif column == "dialogue":
+        query = list(engine.execute(f"SELECT `Dialogue` FROM `Starwars`.`Dialogue` WHERE `Dialogue` = '{string}';"))
+        if len(query) > 0:
+            return True
+        else:
+            return False
+        
+    elif column == "episode":
+        query = list(engine.execute(f"SELECT `Name` FROM `Starwars`.`Episode` WHERE `Name` = '{string}';"))
+        if len(query) > 0:
+            return True
+        else:
+            return False
+
+def insertCharacter(string):
+    if check ('character', string):
+        return 'Character already exists'
+    else:
+        engine.execute(f"INSERT INTO `Character` (`Name`) VALUES ('{string}');")
+
